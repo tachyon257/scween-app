1c704db7acb30f2668e75d7c24648ac582ff954d
Tachyon <tachyondadev@gmail.com>
2026-07-04 10:04:52 +0800

    Initial Commit

---
+++ scween app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  scween app
+//
+//  Created by Pratheesh on 4/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ scween app/scween_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  scween_appApp.swift
+//  scween app
+//
+//  Created by Pratheesh on 4/7/26.
+//
+
+import SwiftUI
+
+@main
+struct scween_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
