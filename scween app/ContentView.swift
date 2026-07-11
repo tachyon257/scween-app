7a286930346d61fbfa3adede5eb93e4babae2a51
Tachyon <tachyondadev@gmail.com>
2026-07-11 10:19:20 +0800

    kjhohiui

--- scween app/ContentView.swift
+++ scween app/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color.blue
+            Color.red
                 .ignoresSafeArea()
             Text("Pratheesh, Katie")
                 .font(.largeTitle)
